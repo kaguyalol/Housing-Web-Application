da507d245411da27e642a0e56c59720724daaa26
mchen <cmm95@outlook.com>
2019-07-18 16:30:12 -0700

    update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var campgroundsRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index");
 
 // seedDB();
-// mongodb+srv://dbUser:12345droeen@cluster0-uwzr8.mongodb.net/test?retryWrites=true&w=majority
 mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
 	useCreateIndex: true
